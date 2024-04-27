8c8cb39c23e516d364e42c1d9976cddf6cb64212
blackisme1 <mskim020815@gmail.com>
2024-04-27 09:15:11 +0900

    a

--- tools/bin/xml2env.pl
+++ tools/bin/xml2env.pl
@@ -12,16 +12,16 @@
 #  in whole or in part, without the prior written consent of Nintendo.
 #
 #  $Log: xml2env.pl,v $
-#  Revision 1.6  2005/10/27 09:14:50  okubata_ryoma
-#  微修正
+#  Revision 1.6  10/27/2005 09:14:50  okubata_ryoma
+#  Minor revisions
 #
-#  Revision 1.5  2005/10/27 08:21:25  okubata_ryoma
-#  微修正
+#  Revision 1.5  10/27/2005 08:21:25  okubata_ryoma
+#  Minor revisions
 #
-#  Revision 1.4  2005/10/27 07:53:19  okubata_ryoma
-#  xml:parserを使用
+#  Revision 1.4  10/27/2005 07:53:19  okubata_ryoma
+#  Use xml:parser
 #
-#  初期版
+#  Initial version
 #
 #  $NoKeywords: $
 #---------------------------------------------------------------------------
@@ -29,16 +29,16 @@
 
 use strict;
 
-# XML::Parserが使用可か調べる
+# Check availability of XML::Parser.
 if (eval "require XML::Parser; return 1;" != 1){
-    printf "cygwin のセットアップでPerlの最新版とexpatライブラリを入れて、XML::Parserを使用できる環境にしてからもう一度実行してみてください。\n";
+    printf "Use cygwin setup to insert the newest version of Perl and the expat library. Set up an environment that allows you to use XML::Parser. Then run again. \n";
     exit;
 }
 
 require Getopt::Std;
 require XML::Parser;
 
-# ハンドラルーチンへの参照をパラメータに指定して、パーサを初期化する
+# Initialize parser by specifying access to the handler routine as a parameter.
 my $parser = XML::Parser -> new(Handlers => {
         Init =>    \&handle_doc_start,
         Final =>   \&handle_doc_end,
@@ -46,34 +46,34 @@ my $parser = XML::Parser -> new(Handlers => {
         End =>     \&handle_elem_end,
         Char =>    \&handle_char_data,
 });
-   
+
 my (%opts);
 my $c = Getopt::Std::getopts('o:h', \%opts);
 
 my ($src, $tmp, $line, $resource_name, $resource_count);
 
-# 何のオプションもつけなかったときの出力ファイル名
+# Output file name used when no options have been attached.
 $tmp = $src . '.result.c';
 
 
-# ｈでヘルプ
+# h for help
 if(exists $opts{h} || scalar(@ARGV) < 1){
-    print "コマンドの後に引数を１つ(検索したい .xml ファイル)を入力してください。\n";
-    print "入力例) perl xml2env.pl d:/test/main.xml \n";
-    print "上の例はファイルが d:/test/main.xml にある場合の例です。\n";
-    print "引数に\"-o\"を入れ、その後にファイル名を入力するとそのファイルに出力されます。\n";
-    print "引数に\"-h\"を入れると、簡単な操作説明が表示されます。\n";
+    print "After the command enter one argument (the .xml file you want to search) \n";
+    print "Input example: perl xml2env.pl d:/test/main.xml \n";
+    print "In the above example, the file is in d:/test/ \n";
+    print "If in the argument you enter "-o\" followed by a file name, the script will output to the file. \n";
+    print "If you enter "-h\" in the argument, a simple help menu will be displayed. \n";
     die "$!";
 }
 
 
 
-# オプション-o のあとに出力ファイルの名前を入れる
+# Enter name of output file after the -o option.
 if(exists $opts{o}){
     $tmp = $opts{o};
 }
 
-# オプションなしで参照したいファイル名を入れる
+# Enter the name of the file to be accessed without an option.
 if(@ARGV){
     $src = $ARGV[$resource_count];
 }
@@ -101,7 +101,7 @@ $line .= "\n";
 print   FO  $line;
 
 
-# データを読み込みパーサで処理する
+# Read data and parse
 my $file = $src;
 if($file){
     $parser->parsefile($file);
@@ -117,18 +117,18 @@ exit;
 
 my ($record, $context, $resource_names, $datafile_name);
 
-# ハンドラ
+# Handler
 
-# 処理が始まると同時に、ファイルの先頭を出力する。
+# Output the top of the file at the point processing starts
 sub handle_doc_start{
     print "";
 }
 my ($class_name, $class_context);
-# パーサが新しい要素の開始に出会うたびに呼ばれる。
+# Call the parser each time the start of a new element is encountered.
 sub handle_elem_start{
     my($expat, $name, %atts) = @_;
     $context = $name;
-    
+
     if($name =~ /resource/i){
         $resource_names .= " $atts{'name'},";
         print "ENVResource $atts{'name'}\[\] = {\n";
@@ -138,14 +138,14 @@ sub handle_elem_start{
         $class_name = $atts{'name'};
     }
     else{
-        # ループでdefault_typeに当てはまるモノを探して処理
+        # In the loop, search for items fitting the default type and process
         foreach my $i(keys %default_type){
             if($name =~ /$i/i){
                 $class_context = "\"$class_name\.$atts{'name'}\",\t". "$default_type{$name}";
                 if($name =~ /hexbinary/i){
                     if(exists $atts{'src'}){
                         $datafile_name = $atts{'src'};
-                        # ファイル読み込み用の属性値の初期化
+                        # Initialize attribute values used when loading files.
                         $atts{'src'} = undef;
                     }
                 }
@@ -154,53 +154,53 @@ sub handle_elem_start{
     }
 }
 
-# 要素内の文字データを最新要素のバッファに収納する。
+# Store character data in the element in the latest element buffer.
 sub handle_char_data{
     my($expat, $text) = @_;
-    
+
     $record->{$context} .= $text;
-    
+
 }
 my ($line, $filedata, $char);
-# パーサが新しい要素の終了に出会うたびに呼ばれる。
+# Call the parser each time the end of a new element is encountered.
 sub handle_elem_end{
     my($expat, $name) = @_;
     if($name =~ /resource/i){
         print "    ENV_RESOUCE_END\n};\n\n";
         print   FO  "    ENV_RESOUCE_END\n};\n\n";
     }
-    # 要素名がclassでないとき
+    # When the element name is not 'class'
     elsif($name ne 'class'){
         if($name =~ /hexbinary/i){
-            # 要素名がhexbinaryでかつ、ファイルを読み込む必要があるとき
+            # When the element name is 'hexbinary' and it is necessary to load a file.
             if($datafile_name){
                 open(FP, "<" . $datafile_name) or die ("cannot open $datafile_name to read.");
                 while(!eof(FP)) {
                     $char = getc FP;
-                    # 文字をアスキーコードに変換
+                    # Convert characters to ASCII code.
                     $char = unpack("C", $char);
-                    # アスキーコードを16進に変換
+                    # Convert ASCII code to hexadecimal.
                     $char = sprintf("%x", $char);
                     $filedata .= '\x' . $char;
                 }
                 print "    $class_context( \"$filedata\" ),\n";
                 print   FO  "    $class_context( \"$filedata\" ),\n";
-                # ファイル読み込みフラグの初期化
+                # Initialize file interrupt flag.
                 $datafile_name = undef;
                 $filedata = "";
             }
             else{
                 my @ascii;
                 my $hexdata;
-                # 文字をアスキーコードに変換
+                # Convert characters to ASCII code.
                 @ascii = unpack("C*", $record->{$context});
                 foreach my $i(@ascii){
-                    # アスキーコードを16進に変換
+                    # Convert ASCII code to hexadecimal.
                     $hexdata .= '\x' . sprintf("%x", $i);
                 }
                 print "    $class_context( \"$hexdata\" ),\n";
                 print   FO  "    $class_context( \"$hexdata\" ),\n";
-                # 毎回テキストを初期化する。
+                # Initialize text each time.
                 $record->{$context} = "";
             }
         }
@@ -210,27 +210,27 @@ sub handle_elem_end{
             $record->{$context} =~ s/[\t]/\\t/g;
             print "    $class_context( \"$record->{$context}\" ),\n";
             print   FO  "    $class_context( \"$record->{$context}\" ),\n";
-            # 毎回テキストを初期化する。
+            # Initialize text each time.
             $record->{$context} = "";
         }
         else{
-            # ループでdefault_typeに当てはまるモノを探して処理
+            # In the loop, search for items fitting the default type and process
             foreach my $i(keys %default_type){
                 if($name =~ /$i/i){
                     print "    $class_context( $record->{$context} ),\n";
                     print   FO  "    $class_context( $record->{$context} ),\n";
-                    # 毎回テキストを初期化する。
+                    # Initialize text each time.
                     $record->{$context} = "";
                 }
             }
         }
-        # テキストの初期化
+        # Initialize text.
         $class_context = "";
         $context = "";
     }
 }
 
-# 最後にファイルの終了を出力する。
+# Output end of file last.
 sub handle_doc_end{
     print "ENVResource* resourceArray\[\]={";
     print   FO  "ENVResource* resourceArray\[\]={";
